a806e8cbcbf2139e37c11f9b7037e09f6ac34515
hterence985 <hterence985@gmail.com>
2025-09-17 06:20:56 +0000

    fix bug

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { Signin } from './signin/signin';
-import { Signup } from './signup/signup';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter([
-      { path: '', component: LandingPageComponent },
-      { path: 'signin', component: Signin },
-      { path: 'signup', component: Signup },
-    ]),
-  ],
+  providers: [provideRouter(routes)],
 };
