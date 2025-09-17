ffe671778ac6cbe0b072d9ccc20f7f40b052d2b0
hterence985 <hterence985@gmail.com>
2025-09-17 06:43:49 +0000

    bug fix

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css'],
   standalone: true,
-  imports: [RouterLink]
+  imports: [RouterLink],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingPageComponent {
-
+  scrollTo(section: string) {
+    document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
