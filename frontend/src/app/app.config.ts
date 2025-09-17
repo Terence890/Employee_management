093cf66685f952a42480fef06043a52669da7aca
hterence985 <hterence985@gmail.com>
2025-09-17 06:14:39 +0000

    added the functionality

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignInComponent } from './signin/signin';
+import { SignUpComponent } from './signup/signup';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter([
       { path: '', component: LandingPageComponent },
+      { path: 'signin', component: SignInComponent },
+      { path: 'signup', component: SignUpComponent },
     ]),
   ],
 };
--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
+  standalone: true,
+  imports: [RouterLink, RouterOutlet]
 })
 export class LandingPageComponent {
 
